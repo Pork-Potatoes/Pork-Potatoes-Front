c8db78b54e1e87ce888f0a3a7204310646a699bc
Lynn <dazzlynnnn@gmail.com>
2021-07-15 18:13:42 +0900

    Update Review Component
    리뷰 컴포넌트 완성
    props = { image: 리뷰 사진, content: 리뷰 내용, restaurantName: 식당 이름, university: 학교, tags: 태그 배열, score: 별점 }
    
    * react-icons 설치 필요
    npm install react-icons --save
    참고: https://react-icons.github.io/react-icons/

--- food-major/src/App.js
+++ food-major/src/App.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Review from "./components/Review";
 
+import image from "./assets/reviewImg.png";
+
 class App extends React.Component {
   render() {
     return(
       <div>
         <Header/>
-        <Review/>
+        <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/>
       </div>
     );
   }
