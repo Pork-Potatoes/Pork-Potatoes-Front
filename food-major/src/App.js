8a8a95e8d4080ac9fbcc8bebf0a6a2595bcb966c
Lynn <dazzlynnnn@gmail.com>
2021-07-18 16:29:31 +0900

    Update App.js with Header Component
    Header Component를 App.js에 넣어서 계속 띄우도록 함

--- food-major/src/App.js
+++ food-major/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from "./components/Header";
+
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 import ListPage from './pages/ListPage';
 import DetailPage from './pages/DetailPage';
 import SearchPage from './pages/SearchPage';
 
+
 class App extends React.Component {
   render() {
     return(
-      <div>
-        <Route path="/" component={MainPage} exact={true} />
-        <Route path="/mypage" component={MyPage} />
-        <Route path="/listpage" component={ListPage} />
-        <Route path="/detail" component={DetailPage} />
-        <Route path="/searchpage" component={SearchPage} /></div>
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route path="/" component={MainPage} exact={true} />
+          <Route path="/mypage" component={MyPage} />
+          <Route path="/listpage" component={ListPage} />
+          <Route path="/detail" component={DetailPage} />
+          <Route path="/searchpage" component={SearchPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
