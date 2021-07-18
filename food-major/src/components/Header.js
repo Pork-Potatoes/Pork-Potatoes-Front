4dbb637aebba8354541321a9a40de9932942a80e
Lynn <dazzlynnnn@gmail.com>
2021-07-18 16:27:33 +0900

    Update Header Position

--- food-major/src/components/Header.js
+++ food-major/src/components/Header.js
@@ -6,6 +6,7 @@ import search from "../assets/search.png";
 import user from "../assets/user.png";
 
 const Wrapper = styled.div`
+    position: fixed;
     background-color: white;
     border-bottom: 1px solid #e0e0e0;
     display: flex;
