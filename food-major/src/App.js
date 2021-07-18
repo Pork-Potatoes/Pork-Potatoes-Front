eebd3ba2f9439d3acb1b129a0de61ecf81be6e7f
LEEYUNJI <yunji965@gmail.com>
2021-07-18 09:06:27 +0900

    Add Main Review List Components
    오늘의 HOT 리뷰, 최신 리뷰 모아보기 구현

--- food-major/src/App.js
+++ food-major/src/App.js
@@ -2,15 +2,64 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import Review from "./components/Review";
-
 import image from "./assets/reviewImg.png";
+
+const Banner = styled.div`
+  background-color: #9FB7B9;
+  width: 100vw;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const HOTReview = styled.div`
+  height: 280px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 150px;
+`
+
+const NewReview = styled.div`
+  height: 560px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 150px;
+`
+
+const Contents = styled.div`
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <Header/>
-        <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/>
+        <Banner></Banner>
+        <HOTReview>
+          <h3 style={{marginBottom:0, marginLeft:15}}>오늘의 HOT 리뷰</h3>
+          <Contents>
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/>
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/>
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/>
+          </Contents>
+        </HOTReview>
+        <NewReview>
+          <h3 style={{marginBottom:0, marginLeft:15}}>최신 리뷰 모아보기</h3>
+          <Contents>
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+            <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+          </Contents>
+        </NewReview>
+        
       </div>
     );
   }
