a8bb88306b691ec7a5629a5cb1ffb93bb1e5eb21
LEEYUNJI <yunji965@gmail.com>
2021-07-28 22:14:49 +0900

    Delete RestuarantInfo Notice Component

--- food-major/src/pages/DetailPage.js
+++ food-major/src/pages/DetailPage.js
@@ -26,18 +26,17 @@ const DetailPage = () => {
         phoneNum="02-363-0113" 
         businessHour="월-금: 11:00 - 21:00"
         snsAccount="https://www.instagram.com/we_saladu/"
-        notice="브레이크 타임 15:00 - 17:00"
         avgScore="4.4"
         scrap="true"/>
       <NewReview>
         <h3 style={{marginBottom:0, marginLeft:15}}>최신 리뷰</h3>
             <Contents>
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
-              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건', '연예인 맛집']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
+              <Review image={image} content="이곳에 리뷰 내용이 들어갑니다" restaurantName="산타비" university="이대" tags={['TV 방영', '비건']} score='4'/> 
             </Contents>
       </NewReview>
     </div>
